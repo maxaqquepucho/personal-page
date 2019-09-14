9f5a9eba72937c2edf24ee8c7203b23057f6743d
maxaqquepucho <maxaqquepucho@gmail.com>
2019-09-14 09:27:25 -0500

    server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3330
+
+app.use(express.static( __dirname + "/../dist/"));
+
+app.get('/', function peticionGet(req, res)  {
+    res.sendFile(__dirname + "/../dist/index.html")
+})
+
+
+
+app.listen(port)
+
+// console.log(process.mainModule.filename)
+
+console.log('Escuchando en el puerto: ' + port);
--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import LayoutMainPerfil from "./layout/MainPerfil/Layout.vue";
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
