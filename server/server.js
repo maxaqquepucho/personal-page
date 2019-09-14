1c0f590cbf11aa7d08bb8a2d1c780980eed31fda
maxaqquepucho <maxaqquepucho@gmail.com>
2019-09-14 09:29:38 -0500

    server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 3330
+const port = process.env.PORT || 3030
 
 app.use(express.static( __dirname + "/../dist/"));
 
