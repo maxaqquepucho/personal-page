742d50c6d623b3545c62806d25732681e7bc8762
maxaqquepucho <maxaqquepucho@gmail.com>
2019-09-21 21:22:57 -0500

    store changes asdhfakljsdfaj

--- src/store.js
+++ src/store.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
         img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/cards/js.jpg',
         name: 'javascript',
         title: 'JavaScript, El lenguaje más importante de la web',
-        techParagraph: `JavaScript es un lenguaje de programación que te permite realizar actividades complejas en una página web —  cada vez más una página web hace más cosas que sólo mostrar información estática — como mostrar actualizaciones de contenido en el momento, interactuar con mapas, animaciones gráficas 2D/3D etc. — puedes estar seguro que JavaScript está involucrado. Es la tercera capa del pastel de los estándares en las tecnologías para la web, dos de las cuales son (HTML y CSS).`,
+        techParagraph: `Es un lenguaje de programación que te permite realizar actividades complejas en una página web —  cada vez más una página web hace más cosas que sólo mostrar información estática — como mostrar actualizaciones de contenido en el momento, interactuar con mapas, animaciones gráficas 2D/3D etc. — puedes estar seguro que JavaScript está involucrado. Es la tercera capa del pastel de los estándares en las tecnologías para la web, dos de las cuales son (HTML y CSS).`,
         projects: [
           {
             img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/project-presentation/vue.png',            
@@ -19,6 +19,18 @@ export default new Vuex.Store({
           {
             img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/project-presentation/vue2.png',
             id: 'id0002'
+          }
+        ]
+      },
+      {
+        img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/cards/ts.jpeg',
+        name: 'typescript',
+        title: 'TypeScript, Los superpoderes de javascript',
+        techParagraph: 'Es un lenguaje de programación libre y de código abierto desarrollado y mantenido por Microsoft. Es un superconjunto de JavaScript, que esencialmente añade tipos estáticos y objetos basados en clases. Anders Hejlsberg, diseñador de C# y creador de Delphi y Turbo Pascal, ha trabajado en el desarrollo de TypeScript.1​ TypeScript puede ser usado para desarrollar aplicaciones JavaScript que se ejecutarán en el lado del cliente o del servidor (Node.js).',
+        projects: [
+          {
+            img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/project-presentation/vue.png',            
+            id: 'id0003'
           }
         ]
       },
@@ -81,22 +93,7 @@ export default new Vuex.Store({
       //       img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/square/node-square.png'
       //     },
       //   ]
-      // },
-      // {
-      //   img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/cards/ts.jpeg',
-      //   name: 'typescript',
-      //   projects: [
-      //     {
-      //       img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/square/dart-square.jpg'
-      //     },
-      //     {
-      //       img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/square/flutter-square.png'
-      //     },
-      //     {
-      //       img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/square/node-square.png'
-      //     },
-      //   ]
-      // },
+      // },      
       // {
       //   img: 'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/personal-page/cards/vue.png',
       //   name: 'vuejs',
@@ -182,6 +179,40 @@ export default new Vuex.Store({
           {
             name: 'Nodejs',
             url: 'https://nodejs.org'
+          },
+        ]
+      },
+      {
+        name: 'API gallery-photo',
+        id: 'id0003',
+        title: 'API de una galeria de fotos',
+        paragraph: `En este proyecto hey participado realizando la maquetación completa de la 
+                    aplicación posteriormente tambien hey desarrollado algunos servicios para ello. La aplicación esta desarrollada
+                    en <a href="https://vuejs.org/" target="_blank"> Vuejs </a> gracias a este framework progresivo se mantiene una gran experiencia en performas UX.
+                  `,
+        images: [
+          'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/photogallery-api/1.png',
+          'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/photogallery-api/2.png',
+          'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/photogallery-api/3.png',
+          'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/photogallery-api/4.png',
+          'https://portafolio-s3-demo.s3-sa-east-1.amazonaws.com/photogallery-api/5.png'
+        ],
+        techs: [
+          {
+            name: 'Nodejs',
+            url: 'https://nodejs.org'
+          },
+          {
+            name: 'TypeScript',
+            url: 'https://www.typescriptlang.org/',
+          },
+          {
+            name: 'Express',
+            url: 'https://expressjs.com'
+          },
+          {
+            name: 'Postman',
+            url: 'https://www.getpostman.com'
           },
         ]
       }
