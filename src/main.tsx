9ac9ba70e71a1e3e0fcdfb332fd15e63b44c750b
Max Aqquepucho <maxaqquepucho@gmail.com>
2024-09-18 22:26:45 -0500

    feat: delete and add space

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@/assets/scss/main.scss'
+
 import '@fontsource-variable/onest'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
     <App />
-
   </StrictMode>
 )
