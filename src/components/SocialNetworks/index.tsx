cca9713927fb28028a3fc9efbc68b6fa63a920eb
Max Aqquepucho <maxaqquepucho@gmail.com>
2024-09-23 22:37:46 -0500

    feat: add space

--- src/components/SocialNetworks/index.tsx
+++ src/components/SocialNetworks/index.tsx
@@ -8,6 +8,7 @@ import {
 
 import './styles.scss'
 
+
 export default function SocialNetworks() {
   const socialNetworks = [
     {
--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,6 @@ import '@fontsource-variable/onest'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    
   </StrictMode>
 )
